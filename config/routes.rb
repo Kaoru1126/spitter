c04f3361b29dcc68e66091ea1d5f6d477fd285da
Kaoru1126 <chocolate.k1126@gmail.com>
2017-07-08 14:57:03 +0900

    for adding moment action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'tweets#index'
-  resources :tweets, only: [:index, :new, :create]
+
+  resources :tweets, only: [:index, :new, :create] do
+    collection do
+      get 'moment'
+    end
+  end
+
   resources :users, only: :show
+  resources :relations, only: [:index, :create, :destroy]
+
 end
