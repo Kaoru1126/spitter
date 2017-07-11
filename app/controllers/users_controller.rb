935917b429ef1b9ab94ec0fd2adcd5e225bc22ae
Kaoru1126 <chocolate.k1126@gmail.com>
2017-07-11 19:19:42 +0900

    added for making following user's list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,32 +1,9 @@
 class UsersController < ApplicationController
-  
+  before_action :set_stats, only: [:show, :following]
+
   def show
-    @myTweets = current_user.tweets.order("created_at DESC")
-    @myTweetCount = @myTweets.count
-
-# フォローカウント
-    @myFollowings = current_user.relations
-    @myFollowingsCount = @myFollowings.count
-    if @myFollowingsCount == nil
-      return 0
-    end
-
-# フォロワーカウント(ダメロジック)
-    # allFollowers = Relation.all
-    # myFollowers = allFollowers.where(following_id: current_user.id)
-    # @myFollowersCount = myFollowers.count
-    # if @myFollowersCount == nil
-    #   return 0
-    # end
-
-# フォロワーカウント
-    allFollowers = Relation.where(following_id: current_user.id)
-    @myFollowersCount = allFollowers.count
-    if @myFollowersCount == nil
-      return 0
-  end
-
 # recommendsだす
+  binding.pry
     @followedUsers = Relation.where(user_id: current_user.id)
     following_ids = []
     following_ids << current_user.id
@@ -36,4 +13,37 @@ class UsersController < ApplicationController
     @recommends = User.where.not(id:following_ids).limit(3)
   end
 
+  def following
+    binding.pry
+    followingIDs = []
+    @allFollowings = Relation.where(user_id: current_user.id)
+    @allFollowings.each do |follows|
+    followingIDs << follows.following_id
+    end
+    @nameBox = []
+    @followingsFromUser = User.where(id:followingIDs)
+    @followingsFromUser.each do |userName|
+    @nameBox << userName
+    end
+  end
+
+private
+
+  def set_stats
+    # ツイートカウント
+    @myTweets = current_user.tweets.order("created_at DESC")
+    @myTweetCount = @myTweets.count
+    # フォローカウント
+    @myFollowings = current_user.relations
+    @myFollowingsCount = @myFollowings.count
+    if @myFollowingsCount == nil
+      return 0
+    end
+    # フォロワーカウント
+    allFollowers = Relation.where(following_id: current_user.id)
+    @myFollowersCount = allFollowers.count
+    if @myFollowersCount == nil
+      return 0
+    end
+  end
 end
