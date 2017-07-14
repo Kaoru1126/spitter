38eb468516c6f5fac32b0dbc775fa07e7311be17
Kaoru1126 <chocolate.k1126@gmail.com>
2017-07-14 21:29:49 +0900

    uploader

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:intro, :avatar, :name])
+  end
 
 end
