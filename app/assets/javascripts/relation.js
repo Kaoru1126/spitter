9e7a7f305d311e3bd7bcd2c06cd737b2d1001d5b
Kaoru1126 <chocolate.k1126@gmail.com>
2017-07-16 02:35:58 +0900

    added some hover actions

--- app/assets/javascripts/relation.js
+++ app/assets/javascripts/relation.js
@@ -51,6 +51,37 @@ $(function() {
     });
   });
 
+// レコメンドのホバーと色変え
+$('.recomendBTNHover').hover(function() {
+    console.log("recomend!");
+   $(this).find('a').text("フォローする");
+   $(this).css('background-color', '#46D89E').css('color', '#ffffff');
+  }, function(){
+   $(this).find('a').text("フォローする");
+   $(this).css('background-color', '#ffffff').css('color', '#21A700');
+   console.log("white!!");
+  });
+
+// ここいる？TRTパイセンに相談
+  // $('.followTrigger').on('click', function() {
+  //   console.log("clicked")
+  //   var parentBox = $(this).closest('.recomendUsers--area')
+  //   var userId = $(this).attr("user-id");
+  //   var followingId = $(this).attr("following-id");
+  //   console.log(followingId);
+  //   $.ajax({
+  //     type: 'POST',
+  //     url: '/relations',
+  //     data: {user_id:userId, following_id:followingId}
+  //   })
+  //   .done(function(data){
+  //     console.log(this)
+  //     parentBox.remove();
+  //   })
+  //   .fail(function(){
+  //     alert('error');
+  //   });
+  // });
 
 });
 
