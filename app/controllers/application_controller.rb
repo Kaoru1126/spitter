46b8d2c7273fca8dd59b75cc3fa2d1c076231f09
Kaoru1126 <chocolate.k1126@gmail.com>
2017-07-18 20:39:15 +0900

    minor fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :configure_permitted_parameters_edit, if: :devise_controller?
+  before_action :request_path
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
@@ -12,4 +13,12 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:intro, :avatar, :name])
   end
 
+  def request_path
+    @path = controller_path + '#' + action_name
+    def @path.is(*str)
+        str.map{|s| self.include?(s)}.include?(true)
+    end
+  end
+
 end
+
