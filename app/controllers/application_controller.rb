ddc64aa0ed4ff4df92156e5bc915c0138d199074
Kaoru1126 <chocolate.k1126@gmail.com>
2017-07-24 20:45:04 +0900

    getting image files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :configure_permitted_parameters_edit, if: :devise_controller?
   before_action :request_path
+  before_action :set_user_stats, except:[:new, :moment, :index, :create, :search]
+  before_action :imageBox
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
@@ -18,6 +20,24 @@ class ApplicationController < ActionController::Base
     def @path.is(*str)
         str.map{|s| self.include?(s)}.include?(true)
     end
+  end
+
+  def set_user_stats
+    @user = User.find(params[:id])
+    @followedUsers = Relation.where(user_id: current_user.id)
+    @following_ids = []
+    @following_ids << current_user.id
+    @followedUsers.each do |followed|
+      @following_ids << followed.following_id
+
+    @numOfTweets = @user.tweets.count
+    @numOfFollows = @followedUsers.count
+    @numOfFollowers = @following_ids.count
+    end
+  end
+
+  def imageBox
+    @imageTweets = Tweet.includes(:user).where.not(image: nil).order("created_at DESC").limit(6)
   end
 
 end
