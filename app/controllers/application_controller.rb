fd99a02fc0a15c6e1d77a71b5f14606609449667
Kaoru1126 <chocolate.k1126@gmail.com>
2017-07-26 15:45:57 +0900

    deleted unnecessary lines

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,6 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :configure_permitted_parameters_edit, if: :devise_controller?
   before_action :request_path
-  before_action :set_user_stats, except:[:new, :moment, :index, :create, :search, :destroy, :edit, :update]
   before_action :imageBox
 
   def configure_permitted_parameters
@@ -19,26 +18,6 @@ class ApplicationController < ActionController::Base
     @path = controller_path + '#' + action_name
     def @path.is(*str)
         str.map{|s| self.include?(s)}.include?(true)
-    end
-  end
-
-  def set_user_stats
-    @user = User.find(params[:id])
-    @followedUsers = Relation.where(user_id: current_user.id)
-    @following_ids = []
-    @following_ids << current_user.id
-    @followedUsers.each do |followed|
-      @following_ids << followed.following_id
-
-    @numOfTweets = @user.tweets.count
-    @numOfFollows = @followedUsers.count
-
-    # フォロワーカウント
-    allFollowers = Relation.where(following_id: @user.id)
-    @myFollowersCount = allFollowers.count
-      if @myFollowersCount == nil
-      return 0
-      end
     end
   end
 
